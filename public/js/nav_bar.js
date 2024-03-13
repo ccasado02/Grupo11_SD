004fd59c2f18c0bc85b569273ec45fbd6eefc69d
ccasado02 <144791375+ccasado02@users.noreply.github.com>
2024-03-13 12:43:17 +0100

    Add files via upload

--- public/js/nav_bar.js
+++ public/js/nav_bar.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var currentPage = window.location.href;
+    // Obtener el nombre de la página actual
+    var currentPage = location.pathname.split("/").pop();
+
+    // Obtener todos los enlaces de la barra de navegación
     var navLinks = document.querySelectorAll(".navbar a");
 
+    // Iterar sobre los enlaces y comparar con la página actual
     navLinks.forEach(function(link) {
-        if (link.href === currentPage) {
-            link.classList.add("active");
+        var linkPage = link.getAttribute("href").split("/").pop();
+
+        if (linkPage === currentPage) {
+            // Aplicar estilo al enlace de la página actual
+            link.style.color = "#fff"; // Cambiar el color del texto
+            link.style.background = "#6ca254"; // Cambiar el color de fondo
         }
     });
 });
+
