9d70710272c0c4b1531137b4f0fe75afe9509878
luc03 <144791350+luc03@users.noreply.github.com>
2024-04-01 14:09:06 +0200

    Add files via upload

---
+++ src/main/java/com/urjc/grupo11/practica1/LICENSETYPE.java
@@ -0,0 +1,6 @@
+package com.urjc.grupo11.practica1;
+
+public enum LICENSETYPE {
+        PERSONAL,
+        COMMERCIAL
+}
---
+++ src/main/java/com/urjc/grupo11/practica1/License.java
@@ -0,0 +1,66 @@
+package com.urjc.grupo11.practica1;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class License {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long userId;
+    private Long beatId;
+    private LocalDate buyDate;
+    private LICENSETYPE licenseType;
+
+    public License(){}
+
+    public License(Long userId, Long beatId, LocalDate buyDate, LICENSETYPE licenseType){
+        super();
+        this.userId=userId;
+        this.beatId=beatId;
+        this.buyDate=buyDate;
+        this.licenseType=licenseType;
+    }
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getBeatId() {
+        return this.beatId;
+    }
+
+    public void setBeatId(Long beatId) {
+        this.beatId = beatId;
+    }
+
+    public LocalDate getBuyDate() {
+        return this.buyDate;
+    }
+
+    public void setBuyDate(LocalDate buyDate) {
+        this.buyDate = buyDate;
+    }
+    public LICENSETYPE getLicenseType(){
+        return this.licenseType;
+    }
+    public void setLicenseType(LICENSETYPE licenseType){
+        this.licenseType = licenseType;
+    }
+}
