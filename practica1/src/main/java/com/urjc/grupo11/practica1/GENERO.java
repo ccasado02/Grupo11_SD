5a97b617511b7083dea77d64955c0e4ba0bc0529
amartine2021 <144791327+amartine2021@users.noreply.github.com>
2024-04-02 19:12:10 +0200

    Delete practica1/src directory

--- practica1/src/main/java/com/urjc/grupo11/practica1/GENERO.java
+++
@@ -1,5 +0,0 @@
-package com.urjc.grupo11.practica1;
-
-public enum GENERO {
-    POP, TRAP, RAP, RB, REGGAETON, ELECTRONICA
-}
--- practica1/src/main/resources/static/js/toggle.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var menuToggle = document.querySelector(".menu-toggle");
-    var navbar = document.querySelector(".navbar");
-
-    menuToggle.addEventListener("click", function() {
-        navbar.classList.toggle("active"); // Alternar la clase "active" en la barra de navegación
-    });
-});
