b9bbeaaf4abecc15b46f7762c65b9f612d45c65c
luc03 <144791350+luc03@users.noreply.github.com>
2024-04-02 12:37:52 +0200

    Delete practica1/src/src directory

--- practica1/src/src/main/resources/static/js/check_session.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    var user = sessionStorage.getItem('user');
-    if (user) {
-        document.getElementByClass('navbar').style.display = 'none';
-        var userElement = document.createElement('span');
-        userElement.textContent = 'Bienvenido, ' + user;
-        document.querySelector('.navbar').appendChild(userElement);
-    }
-};
